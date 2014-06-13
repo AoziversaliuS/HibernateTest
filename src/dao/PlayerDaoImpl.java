d9248e34d02d57be2f7e58751ccff4693054b5cc
AoziversaliuS <1220310838@qq.com>
2014-06-13 12:25:30 +0800

    备份

---
+++ src/dao/PlayerDaoImpl.java
@@ -0,0 +1,17 @@
+package dao;
+
+import javax.annotation.Resource;
+
+import org.hibernate.SessionFactory;
+
+import pojo.Player;
+
+public class PlayerDaoImpl {
+
+	private SessionFactory sf;
+	
+	public void add(Player p){
+		
+	}
+
+}
